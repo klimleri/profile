9aea50c17a5ae295c2c9094d5c9de6407262a43f
valeriia.klimova <valeriia.klimova@x2sy.com>
2020-05-24 17:26:18 +0300

    add files

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+const gulp = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const browserify = require('gulp-browserify');
+const babelify = require('babelify');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const svgSprite = require('gulp-svg-sprite');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const ghPages = require('gulp-gh-pages');
+
+function html() {
+    return src('src/index.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('build'))
+        .pipe(browserSync.stream());
+}
+function css() {
+    return src('src/scss/index.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(cleanCss())
+        .pipe(rename('main.css'))
+        .pipe(dest('build'))
+        .pipe(browserSync.stream());
+}
+function js() {
+    return src('src/js/main.js')
+        .pipe(browserify({ transform: [babelify.configure({ presets: ['@babel/preset-env'] })] }))
+        .pipe(uglify({
+            toplevel: true
+        }))
+        .pipe(rename('main.bundle.js'))
+        .pipe(dest('build'))
+        .pipe(browserSync.stream());
+}
+function images() {
+    return src('src/assets/*')
+        .pipe(imagemin())
+        .pipe(dest('build/assets'))
+        .pipe(browserSync.stream());
+}
+function clean(){
+    return del(['./build/*']);
+}
+function sprite() {
+    return src('src/assets/icons/*.svg')
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: '../sprite.svg'
+                }
+            },
+        }
+        ))
+        .pipe(dest('build/assets/icons/'))
+}
+function dev(){
+    browserSync.init({
+        server: './build'
+    });
+    watch('src/*.html', html);
+    watch('src/scss/*.scss', css);
+    watch('src/js/**/*.js', js);
+    watch('src/assets/*', images);
+}
+function build(){
+    return series(clean, parallel(js, css), images, sprite, html);
+}
+
+exports.build = build();
+exports.dev = series(clean, build(), dev);
+
+gulp.task('deploy', function() {
+    return gulp.src('./build/**/*')
+        .pipe(ghPages());
+});
